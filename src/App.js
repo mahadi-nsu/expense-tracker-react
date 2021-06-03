822e2543affe871598a803b73e4bca0df01381e2
mahadi-nsu <mahadi.hassan@northsouth.edu>
2021-06-03 10:13:37 +0600

    expense list rendered and list shown

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 
+import NewExpense from "./components/NewExpense/NewExpense";
+import Expenses from "./components/Expenses/Expenses";
+
 const DUMMY_EXPENSES = [
   {
     id: "e1",
@@ -23,7 +26,20 @@ const DUMMY_EXPENSES = [
 ];
 
 const App = () => {
-  return <div>Here we go!</div>;
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
+
+  return (
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
+    </div>
+  );
 };
 
 export default App;
